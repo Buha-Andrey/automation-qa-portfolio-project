88b3d7cfce743577127dd5d4314b66b3bd1f92d6
AndriiBuha <buha.andrey@gmail.com>
2026-09-25 14:28:51 +0300

    test data is restructured to array of objects and moved to separate file

---
+++ test-data/test-data.ts
@@ -0,0 +1,26 @@
+export const  roleTestData = [
+  {
+    role: "tester",
+    description:
+      "Explore the software functionality to compare with stakeholder needs and requirements, identifying bugs, verifying fixes, and raising unexpected risks and issues.",
+    expectedResponseRoleText: "tester",
+  },
+  {
+    role: "programmer",
+    description:
+      "Design, write, test, and maintain code to build software applications. Transform requirements into functional solutions using various programming languages and frameworks.",
+    expectedResponseRoleText: "programmer",
+  },
+  {
+    role: "manager",
+    description:
+      "Lead and coordinate the development team, manage project timelines, allocate resources, and be responsible for delivery of software projects while fostering team collaboration.",
+    expectedResponseRoleText: "manager",
+  },
+  {
+    role: "all",
+    description:
+      "Take on multiple responsibilities across management, development, and testing. Perfect for versatile professionals who enjoy wearing different hats and contributing to all aspects of software delivery.",
+    expectedResponseRoleText: "generalist (one who does it all)",
+  },
+];
