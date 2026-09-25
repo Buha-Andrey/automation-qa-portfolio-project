87f3af3dc2cad3c4c894e7eb929a5b80f3276ea4
AndriiBuha <buha.andrey@gmail.com>
2026-09-25 14:33:03 +0300

    created fixture to set Viewport and to check console errors during tests

---
+++ fixtures/fixtureBase.ts
@@ -0,0 +1,17 @@
+import { test as base, ConsoleMessage } from "@playwright/test";
+import { MultipleElementsPage } from "../pages/MultipleElementsPage";
+
+export const test = base.extend({
+  page: async ({ page }, use) => {
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    page.on("console", async (msg:ConsoleMessage) => {
+        if (msg.type() === "error"){
+            throw new Error("Error");
+        }
+    })
+
+    // additional needed logic
+
+    await use(page);
+  },
+});
